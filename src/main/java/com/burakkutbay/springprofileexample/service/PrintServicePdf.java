e2ee0597d37e924c68485b53ec3bc6ab20b8a9e9
Burak <brkktby@gmail.com>
2017-11-29 12:11:41 +0300

    Print Service Created Implement Method

---
+++ src/main/java/com/burakkutbay/springprofileexample/service/PrintServicePdf.java
@@ -0,0 +1,14 @@
+package com.burakkutbay.springprofileexample.service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("pdf")
+public class PrintServicePdf implements PrintService{
+
+    @Override
+    public String printHello(String name) {
+        return "Pdf Çıktı" + name ;
+    }
+}
