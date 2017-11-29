ab62ec7d5f6c68714e0fdbdb577b5dfe37545aa6
Burak <brkktby@gmail.com>
2017-11-29 12:06:02 +0300

    Print Controller Created

--- src/main/java/com/burakkutbay/springprofileexample/controller/PrintController.java
+++ src/main/java/com/burakkutbay/springprofileexample/controller/PrintController.java
@@ -1,4 +1,18 @@
 package com.burakkutbay.springprofileexample.controller;
 
+import com.burakkutbay.springprofileexample.service.PrintService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
 public class PrintController {
+    @Autowired
+    private PrintService printService;
+
+    public String hello(@PathVariable(value = "name") String name){
+        return printService.printHello(name);
+    }
 }
